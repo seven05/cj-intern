f10300a76c8f0fee0cbac41fc2ee1ab8c463a8b7
seven05 <seven05@unist.ac.kr>
2022-08-30 17:45:47 +0900

    Add files via upload

---
+++ PARK/soccer_edit2/contours.py
@@ -0,0 +1,36 @@
+from distutils.log import error
+import cv2
+import os
+import time
+import numpy as np
+
+try:
+    if not os.path.exists('./contours'):
+        os.makedirs('./contours')
+except OSError:
+    print('error : creating dir contours')
+
+for t in range(600,6000):
+    img = cv2.imread('./test' + "/test3_%d.jpg" %t, 1)
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #contours, hierachy = cv2.findContours(img_gray, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #img_con = cv2.drawContours(img, contours, -1, (255,0,0), 1)
+    #cv2.imwrite('./contours' + "/contours_%d.jpg" %t, img_con)
+    contours = cv2.findContours(img_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+    if len(contours) == 0 :
+        continue
+    big_contour = max(contours, key=cv2.contourArea)
+    result = np.zeros_like(img)
+    result = cv2.drawContours(result, [big_contour], 0, (255,255,255), cv2.FILLED)
+    cv2.imwrite('./contours' + "/contours3_%d.jpg" %t, result)
+'''
+result_gray = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
+contours2 = cv2.findContours(result_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contour2 = contours2[0]
+epsilon = cv2.arcLength(contour2, True)
+approx = cv2.approxPolyDP(contour2, epsilon, True)
+result2 = np.zeros_like(img)
+result2 = cv2.drawContours(result2, [approx], -1, (0,255,0), 3)
+cv2.imwrite('./contours' + "/contours3_%d.jpg" %t, result2)
+'''
---
+++ PARK/soccer_edit2/test.py
@@ -0,0 +1,43 @@
+from distutils.log import error
+import cv2
+import os
+import time
+filepath = 'video\P470472958_EPI0002_01_t35.mp4'
+cap = cv2.VideoCapture(filepath)
+if not cap.isOpened():
+    print("Could not Open :", filepath)
+    exit(0)
+#cap.set(cv2.CAP_PROP_POS_FRAMES,20000)
+fps = cap.get(cv2.CAP_PROP_FPS)
+fps = round(fps)
+frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+duration = frame/fps
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+try:
+    if not os.path.exists('./Captures'):
+        os.makedirs('./Captures')
+except OSError:
+    print('error : creating dir Captures')
+
+start = time.time()
+count = 0
+while(cap.get(cv2.CAP_PROP_POS_FRAMES) <= frame-100):
+    ret, img = cap.read()
+    if(int(cap.get(cv2.CAP_PROP_POS_FRAMES)) % fps == 0):
+        #img = cv2.resize(img,dsize=(1280,720))
+        img_crop = img[0:int(height/2),0:int(width/2)]
+        #edges = cv2.Canny(img_crop, 100,600)
+        #cv2.imwrite('./Captures' + "/edges_%d.jpg" %count, edges)
+        img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+        ret, img_thr = cv2.threshold(img_gray,127,255,cv2.THRESH_BINARY_INV)
+        #cv2.imwrite('./Captures' + "/thresh_%d.jpg" %count, img_thr)
+        contours, hierachy = cv2.findContours(img_thr, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        img_con = cv2.drawContours(img_crop, contours, -1, (255,0,0), 1)
+        cv2.imwrite('./Captures' + "/contours_%d.jpg" %count, img_con)
+        count +=1
+print("time :", time.time() - start)
+
+cap.release()
+exit()
