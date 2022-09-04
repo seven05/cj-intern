e510903c9c8a2fe0378b8d92c99e7e9b2e370833
seven05 <ock1997@gmail.com>
2022-09-04 18:10:53 +0900

    edge cutting add

--- PARK/soccer_edit3/fill.py
+++ PARK/soccer_edit3/fill.py
@@ -8,6 +8,55 @@ try:
 except OSError:
     print('error : creating dir fill')
 
+def edge_cut(img):
+        status = 0
+        x_min,y_min,x_max,y_max = 0,0,0,0
+        cols, rows = img.shape
+        for x in range(rows):
+            for y in range(cols):
+                if (img[y][x] == 255):
+                    x_min = x
+                    status = 1
+                    break
+                if (status == 1):
+                    break
+        status = 0
+
+        for y in reversed(range(cols)):
+            for x in reversed(range(rows)):
+                if (img[y][x] == 255):
+                    x_max = x
+                    y_max = y
+                    status = 1
+                    break
+                if (status == 1):
+                    break
+        status = 0
+
+        for y in range(cols):
+            for x in range(rows):
+                if (img[y][x] == 255):
+                    y_min = y
+                    status = 1
+                    break
+                if (status == 1):
+                    break
+        status = 0
+
+        for x in reversed(range(rows)):
+            for y in reversed(range(cols)):
+                if (img[y][x] == 255):
+                    if (y >= y_max):
+                        y_max = y
+                    if (x >= x_max):
+                        x_max = x
+                    status = 1
+                    break
+                if (status == 1):
+                    break
+        status = 0
+        return x_min,y_min,x_max,y_max
+
 for x in range(1500,6400):
     for i in range(0,4):
         if(os.path.isfile("number/%d_num_%d.jpg" %(x,i)) == False):
@@ -15,19 +64,12 @@ for x in range(1500,6400):
         img = cv2.imread('number\%d_num_%d.jpg' %(x,i), cv2.IMREAD_GRAYSCALE)
         h, w = img.shape
         if(img[0][0] > 128 and img[h-1][0] > 128 and img[0][w-1] > 128 and img[h-1][w-1] > 128):
-            img_thr = cv2.threshold(img, 128, 0, cv2.THRESH_BINARY)[1]
+            img_thr = cv2.threshold(img, 80, 0, cv2.THRESH_BINARY)[1]
         else:    
-            img_thr = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)[1]
-        #img_edge = cv2.Canny(img_thr,100,600)
-        '''
-        contours = cv2.findContours(img_thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours = contours[0] if len(contours) == 2 else contours[1]
-        if(len(contours) == 0):
+            img_thr = cv2.threshold(img, 156, 255, cv2.THRESH_BINARY)[1]
+        x_min,y_min,x_max,y_max = edge_cut(img_thr)
+        if(x_min == x_max or y_min == y_max):
             continue
-        big_contour = max(contours, key=cv2.contourArea)
-        result = np.zeros_like(img)
-        cv2.drawContours(result, [big_contour], 0, (255,255,255), cv2.FILLED)
-        '''
-        cv2.imwrite('fill\%d_%d.jpg' %(x,i), img_thr)
-        
+        img_edge_cut = img_thr[y_min:y_max,x_min:x_max]
+        cv2.imwrite('fill\%d_%d.jpg' %(x,i), img_edge_cut)       
 exit()
