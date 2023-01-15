bc5f3f1be3ca3460ecfb960ad23a938171bc3ec1
uni20181283 <stillastudent@unist.ac.kr>
2023-01-15 20:43:55 +0900

    add demo

---
+++ HWANG/Streamlit_ex/yolor_streamlit_vehicle_Detection/yolor.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dccae6f8a899ed0c2a40b4cfb0c342472b56f1e4047eadcab346dd853d159903
+size 21739
---
+++ HWANG/Streamlit_ex/yolor_streamlit_vehicle_Detection/yolor_streamlit_demo.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6bfedc3e51b5f782e34d0eb6a110e600aed4df40a366570859afb05bc141a75c
+size 3700
